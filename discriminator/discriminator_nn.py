1e9cb5b05c7ba7bb7952938c812366ce383b5dce
MarvinLvn <marvinlavechin@gmail.com>
2026-01-29 12:43:33 +0100

    More code cleaning

--- discriminator/discriminator_nn.py
+++
@@ -1,7 +0,0 @@
-from torch import nn
-
-class ArticuloGANNN(nn.Module):
-    def __init__(self, discriminator, generator):
-        super(ArticuloGANNN, self).__init__()
-        self.discriminator = discriminator
-        self.generator = generator
